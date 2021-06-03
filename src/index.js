27d135ba35bbd9d39b719e666adca67f4c491cf0
Rahul Tarak <cryogenicplanet@gmail.com>
2021-06-03 23:44:34 +0530

    🐛 Ask pnpm to shamefully hoist on CI

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ module.exports = {
     try {
       if (process.env.CI) {
         await run.command(
-          'npx pnpm install -r --store=node_modules/.pnpm-store'
-        )
+          "npx pnpm install -r --shamefully-hoist --store=node_modules/.pnpm-store"
+        );
       } else {
-        status.show({ summary: 'CI is false, skipping pnpm install.' })
+        status.show({ summary: "CI is false, skipping pnpm install." });
       }
 
-      status.show({ summary: 'Installed pnpm!' })
+      status.show({ summary: "Installed pnpm!" });
     } catch (e) {
-      build.failBuild(`An error occured while installing pnpm: ${e.message}`)
+      build.failBuild(`An error occured while installing pnpm: ${e.message}`);
     }
   }
-}
+};
